50a9dac42d7728b714026282f14b687b39a01d4a
Simon Kennedy <sffjunkie+code@gmail.com>
2022-10-27 16:12:44 +0100

    Changed to relative imports

--- matrix/__init__.py
+++ matrix/__init__.py
@@ -1,6 +1,6 @@
-from matrix.base import Matrix
-from matrix.matrix1x import Matrix1x2, Matrix1x3
-from matrix.matrix2x import Matrix2x1, Matrix2x2
-from matrix.matrix3x import Matrix3x1, Matrix3x3
+from .base import Matrix
+from .matrix1x import Matrix1x2, Matrix1x3
+from .matrix2x import Matrix2x1, Matrix2x2
+from .matrix3x import Matrix3x1, Matrix3x3
 
 __all__ = [Matrix1x2, Matrix1x3, Matrix2x1, Matrix2x2, Matrix3x1, Matrix3x3, Matrix]
