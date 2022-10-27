eabefc8a444f4f6de4922ef25e577ee23ef6f86e
Simon Kennedy <sffjunkie+code@gmail.com>
2022-10-27 15:44:22 +0100

    Added __repr__ and __rich_repr__

--- matrix/base.py
+++ matrix/base.py
@@ -2,6 +2,13 @@ from typing import Any
 
 
 class Matrix:
+    def __repr__(self):
+        return f"{self.__class__.__name__} = {self.data}"
+
+    def __rich_repr__(self):
+        yield self.__class__.__name__
+        yield "data", self.data
+
     def __eq__(self, other: Any):
         if isinstance(other, self.__class__):
             return self.data == other.data
